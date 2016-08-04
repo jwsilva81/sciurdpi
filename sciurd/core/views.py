158a2f28d3ac4141a5d063037f616ccc436360a7
Patrick Mazulo <pmazulo@gmail.com>
2016-08-04 00:32:15 -0300

    Mudanças realizadas: - Aplicado PEP8 em todo o código - Corrigida identação errada em alguns blocos - Adicionado o form_class em cada uma das views que herdam de CreateView - Implementado o get_context_data para poder inserir o nome correto para aparecer no template de cadastro - Corrigida algumas URLs na função reverse_lazy que não existiam

--- sciurd/core/views.py
+++ sciurd/core/views.py
@@ -1,148 +1,176 @@
-from django.shortcuts import render
-from core.models import Pastor, Esposa, Filho, Igreja, Carro
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
-#from core.forms import PastorForm, EsposaForm, FilhoForm,IgrejaForm, CarroForm
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    TemplateView
+)
 from django.core.urlresolvers import reverse_lazy
-from django.http import HttpResponse
 
-#PASTOR
+from core.models import Pastor, Esposa, Filho, Igreja, Carro
+from core.forms import (
+    PastorForm,
+    EsposaForm,
+    FilhoForm,
+    IgrejaForm,
+    CarroForm,
+)
+
 
 class HomeView(TemplateView):
-	template_name = 'index.html'
+    template_name = 'index.html'
+
+
+class PastorCreateView(CreateView):
+    template_name = 'add.html'
+    form_class = PastorForm
+    model = Pastor
+    success_url = reverse_lazy('pastor-add')
+
+    def get_context_data(self, **kwargs):
+        context = super(PastorCreateView, self).get_context_data(**kwargs)
+        context['cadastro'] = 'Pastor'
+        return context
 
 
 class PastorListView(ListView):
-	template_name = 'view.html'
-	model = Pastor
-	#context_object_name = 'nome,' 'cpf'
-		
+    template_name = 'view.html'
+    model = Pastor
+
 
 class PastorDetailView(DetailView):
-	 template_name = 'detail.html'
-	 model = Pastor
-	
-class PastorCreateView(CreateView):
-	template_name = 'add.html'
-	model = Pastor
-	success_url = reverse_lazy('listar')
+    template_name = 'detail.html'
+    model = Pastor
 
 
 class PastorUpdateView(UpdateView):
-	template_name = 'update.html'
-	model = Pastor
-	success_url = '/pastor/'
+    template_name = 'update.html'
+    model = Pastor
+    success_url = '/pastor/'
 
 
 class PastorDeleteView(DeleteView):
-	template_name = 'delete.html'
-	model = Pastor
-	success_url  = reverse_lazy('pastor-list')
-
-
-#Esposa	
-class EsposaListView(ListView):
-	model = Esposa
-	success_url = reverse_lazy('listar')
-
-
-class EsposaDetailView(DetailView):
-	model = Esposa
+    template_name = 'delete.html'
+    model = Pastor
+    success_url = reverse_lazy('pastor-list')
 
 
 class EsposaCreateView(CreateView):
-	template_name = 'esposa.html'
-	model = Esposa
-	success_url =  reverse_lazy('esposa-list')
+    form_class = EsposaForm
+    template_name = 'esposa.html'
+    model = Esposa
+    success_url = reverse_lazy('esposa-list')
+
+    def get_context_data(self, **kwargs):
+        context = super(EsposaCreateView, self).get_context_data(**kwargs)
+        context['cadastro'] = 'Esposa'
+        return context
+
+
+class EsposaListView(ListView):
+    model = Esposa
+    success_url = reverse_lazy('listar')
+
+
+class EsposaDetailView(DetailView):
+    model = Esposa
 
 
 class EsposaUpdateView(UpdateView):
-	model = Esposa
-	success_url =  reverse_lazy('esposa-list')
+    model = Esposa
+    success_url = reverse_lazy('esposa-list')
+
 
 class EsposaDeleteView(DeleteView):
-	model = Esposa
-	success_url = reverse_lazy('esposa-list')
+    model = Esposa
+    success_url = reverse_lazy('esposa-list')
 
 
-#FILHO
 class FilhoListView(ListView):
-	model = Filho
+    model = Filho
 
 
 class FilhoDetailView(DetailView):
-	model = Filho
+    model = Filho
 
 
 class FilhoCreateView(CreateView):
-	model = Filho
-	success_url = reverse_lazy('filho-list')
+    form_class = FilhoForm
+    model = Filho
+    success_url = reverse_lazy('filho-list')
+    template_name = 'add.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(FilhoCreateView, self).get_context_data(**kwargs)
+        context['cadastro'] = 'Filho'
+        return context
 
 
 class FilhoUpdateView(UpdateView):
-	model = Filho
-	success_url = reverse_lazy('filho-list')
-	
+    model = Filho
+    success_url = reverse_lazy('filho-list')
+
 
 class FilhoDeleteView(DeleteView):
-	model = Filho
-	success_url = reverse_lazy('filho-list')
+    model = Filho
+    success_url = reverse_lazy('filho-list')
 
 
-#Igreja
 class IgrejaListView (ListView):
-	model = Igreja
+    model = Igreja
 
 
 class IgrejaDetailView(DetailView):
-	model = Igreja
+    model = Igreja
 
 
 class IgrejaCreateView(CreateView):
-	model = Igreja
-	success_url = reverse_lazy ('igreja')
+    form_class = IgrejaForm
+    model = Igreja
+    success_url = reverse_lazy('igreja-list')
+    template_name = 'add.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IgrejaCreateView, self).get_context_data(**kwargs)
+        context['cadastro'] = 'Igreja'
+        return context
 
 
 class IgrejaUpdateView(UpdateView):
-	model = Igreja
-	success_url = reverse_lazy	
+    model = Igreja
+    success_url = reverse_lazy
 
 
 class IgrejaDeleteView(DeleteView):
-	model = Igreja
-	success_url = reverse_lazy		
+    model = Igreja
 
-
-
-#Carro
 
 class CarroListView(ListView):
-	model = Carro
+    model = Carro
 
 
 class CarroDetailView(DetailView):
-	model = Carro
+    model = Carro
 
 
 class CarroCreateView(CreateView):
-	model = Carro
-	success_url = reverse_lazy('carro-list')
+    form_class = CarroForm
+    model = Carro
+    success_url = reverse_lazy('carro-list')
+    template_name = 'add.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CarroCreateView, self).get_context_data(**kwargs)
+        context['cadastro'] = 'Carro'
+        return context
 
 
 class CarroUpdateView(UpdateView):
-	model = Carro
-	success_url = reverse_lazy('carro-list')
-		
-	
+    model = Carro
+    success_url = reverse_lazy('carro-list')
+
+
 class CarroDeleteView(DeleteView):
-	model = Carro
-	success_url = reverse_lazy('carro-list')
-
-
-
-
-
-
-
-
-
-
+    model = Carro
+    success_url = reverse_lazy('carro-list')
